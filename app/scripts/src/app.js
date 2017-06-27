7607c9c8025c3c3eb9d00ac6f0856e7ab14a216c
beeth0ven <beeth0vendev@163.com>
2017-06-27 21:26:51 +0800

    Finished

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -3,14 +3,37 @@
  */
 
 import socket from './ws-client';
+import {UserStore} from './storage';
+import {ChatForm, ChatList, promptForUsername} from './dom';
+
+const formSelector = '[data-chat="chat-form"]';
+const inputSelector = '[data-chat="message-input"]';
+const listSelector = '[data-chat="message-list"]';
+
+let userStore = new UserStore('x-chattrbox/u');
+let username = userStore.get();
+if (!username) {
+  username = promptForUsername();
+  userStore.set(username);
+}
 
 class ChatApp {
   constructor() {
+    this.chatForm = new ChatForm(formSelector, inputSelector);
+    this.chatList = new ChatList(listSelector, username);
+
     socket.init('ws://localhost:3001');
     socket.registerOpenHandler(() => {
-      let message = new ChatMessage({ message: 'pow!' });
-      socket.sendMessage(message);
+
+      this.chatForm.init((data) => {
+        let message = new ChatMessage({message: data});
+        socket.sendMessage(message.serialize());
+      });
+      this.chatList.init();
+
       socket.registerMessageHandler((data) => {
+        let message = new ChatMessage(data);
+        this.chatList.drawMessage(message.serialize());
         console.log(data);
       });
     });
@@ -19,7 +42,7 @@ class ChatApp {
 }
 
 class ChatMessage {
-  constructor({message: m, user: u='natman', timestamp: t=(new Date()).getTime()}) {
+  constructor({message: m, user: u=username, timestamp: t=(new Date()).getTime()}) {
     this.message = m;
     this.user = u;
     this.timestamp = t;
@@ -28,7 +51,7 @@ class ChatMessage {
     return {
       user: this.user,
       message: this.message,
-      timestamp: this.t
+      timestamp: this.timestamp
     }
   }
 }
